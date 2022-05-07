b0ed9de0b4d25b868dd357b4d7c78faa19ab259f
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-05-07 19:39:31 +0200

    vyjmuti promenne whoseTurn na globalni

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log('funguju');
 let playerTurn = 'circle';
 
 const myTurn = document.querySelectorAll('button');
+const whoseTurn = document.querySelector('.icon__circle');
 
 for (let i = 0; i < myTurn.length; i++) {
   myTurn[i].addEventListener('click', (event) => {
@@ -10,15 +11,11 @@ for (let i = 0; i < myTurn.length; i++) {
     if (playerTurn === 'circle') {
       event.target.classList.add('board__field--circle');
       playerTurn = 'cross';
-      const whoseTurn = document.querySelector('.icon__circle');
       whoseTurn.src = 'cross.svg';
-      whoseTurn.alt = 'křížik je na tahu';
     } else {
       event.target.classList.add('board__field--cross');
       playerTurn = 'circle';
-      const whoseTurn = document.querySelector('.icon__circle');
       whoseTurn.src = 'circle.svg';
-      whoseTurn.alt = 'kolečko je na tahu';
     }
     myTurn[i].disabled = true;
     if (isWinningMove(myTurn[i]) === true) {
